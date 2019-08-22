938860a0a39111b4aae34e92fb170271f5163a04
Jesper Lehtinen <jsprlehtinen@gmail.com>
2019-08-22 22:13:53 +0200

    basic functions up and running calling SL API to get departures print next departure time

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import { getRealtimeData } from './service'
+import { formatMilliseconds } from './util'
 
-function App() {
+const SOLNA_BUSINESS_PARK = 5119
+const DIRECTION = 2
+const FOUR_HALF_MINUTES = 270000
+
+export const App = () => {
+  const [latestUpdate, setLatestUpdate] = useState(new Date())
+  const [departures, setDepartures] = useState([])
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const response = await getRealtimeData(SOLNA_BUSINESS_PARK)
+        const responseData = response.data.ResponseData
+        setDepartures(responseData.Trams.filter(departure => departure.JourneyDirection === DIRECTION))
+        setLatestUpdate(new Date(responseData.LatestUpdate))
+      } catch (e) {
+        console.error('error fetching data', e)
+      }
+    }
+    fetch()
+  }, [])
+
+  if (!departures || departures.length === 0) {
+    return null
+  }
+
+  let nextDeparture = departures[0]
+  if (new Date(nextDeparture.ExpectedDateTime).getTime() - new Date().getTime() < FOUR_HALF_MINUTES) {
+    nextDeparture = departures[1]
+  }
+  const nextDepartureTime = formatMilliseconds(new Date(nextDeparture.ExpectedDateTime).getTime() - new Date().getTime())
+
   return (
     <div className="App">
+      <p>Antal avgångar: {departures.length}</p>
+      <p>Nästa tåg mot Sickla går om {nextDepartureTime}</p>
+      <p>Senast uppdaterad: {latestUpdate.toLocaleDateString('se-SV', {timeStyle: 'medium'})}</p>
     </div>
-  );
+  )
 }
-
-export default App;
