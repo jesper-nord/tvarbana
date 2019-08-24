67bab2b1f712391981da0d67f82457c3b630ef97
Jesper Lehtinen <jsprlehtinen@gmail.com>
2019-08-25 00:24:53 +0200

    https api

--- src/service.js
+++ src/service.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://api.sl.se/api2'
+const BASE_URL = 'https://api.sl.se/api2'
 const REAL_TIME_API = 'realtimedeparturesV4.json'
 
 export const getRealtimeData = siteId => {
-  return axios.get(`${BASE_URL}/${REAL_TIME_API}?key=${process.env.REACT_APP_SL_API_KEY}&siteid=${siteId}&timewindow=25&bus=false&metro=false&ship=false&train=false`)
+  return axios.get(`${BASE_URL}/${REAL_TIME_API}?key=${process.env.REACT_APP_SL_API_KEY}&siteid=${siteId}&timewindow=30`)
 }
