d2f8aeecc2b77bac885da1a11fd581bcffb38714
Jesper Lehtinen <jsprlehtinen@gmail.com>
2019-08-25 16:54:43 +0200

    stop progress bar on every request done

--- src/service.js
+++ src/service.js
@@ -1,20 +1,29 @@
 import axios from 'axios'
+import uuid from 'uuid/v4'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 
-axios.interceptors.request.use(function (config) {
+let requestIds = new Map()
+
+axios.interceptors.request.use(config => {
+  const id = uuid()
+  config.__id = id
+  requestIds.set(id, NProgress.done)
   NProgress.start()
   setInterval(() => {
     NProgress.inc()
-  }, 150)
+  }, 200)
   return config;
 }, function (error) {
   NProgress.done()
   return Promise.reject(error);
 });
 
-axios.interceptors.response.use(function (response) {
-  NProgress.done()
+axios.interceptors.response.use(response => {
+  const id = response.config.__id
+  const stop = requestIds.get(id)
+  stop()
+  requestIds.delete(id)
   return response;
 }, function (error) {
   NProgress.done()
