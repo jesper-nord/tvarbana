857a8ab3c98f22601d39aed62fbf4a33e3fc0e68
Jesper Lehtinen <jsprlehtinen@gmail.com>
2019-08-25 17:23:36 +0200

    cache bust all requests; move config to separate file

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './app/app'
+import { configAxios } from './config'
+
+configAxios()
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/service.js
+++ src/service.js
@@ -1,40 +1,5 @@
 import axios from 'axios'
-import uuid from 'uuid/v4'
-import NProgress from 'multi-nprogress'
-import 'multi-nprogress/nprogress.css'
-
-const progress = NProgress()
-let requestIds = new Map()
-
-axios.interceptors.request.use(config => {
-  const id = uuid()
-  config.__id = id
-  progress.start()
-  const interval = setInterval(() => {
-    progress.inc()
-  }, 200)
-  requestIds.set(id, interval)
-  return config;
-}, function (error) {
-  progress.done()
-  progress.remove()
-  return Promise.reject(error);
-});
-
-axios.interceptors.response.use(response => {
-  const id = response.config.__id
-  clearInterval(requestIds.get(id))
-  requestIds.delete(id)
-  progress.done()
-  progress.remove()
-  return response;
-}, function (error) {
-  progress.done()
-  progress.remove()
-  return Promise.reject(error);
-});
-
 
 export const getRealtimeDataFor = siteId => {
-  return axios.get('/.netlify/functions/realTimeApi?siteId=' + siteId)
+  return axios.get('/.netlify/functions/realTimeApi?siteId=' + siteId + `&timestamp=${new Date().getTime()}`)
 }
