0d7c333d52ac4feb8315d2c789e4ec99df7fb3fb
Jesper Lehtinen <jsprlehtinen@gmail.com>
2019-08-25 16:42:05 +0200

    adds progress bar

--- src/service.js
+++ src/service.js
@@ -1,4 +1,26 @@
 import axios from 'axios'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+axios.interceptors.request.use(function (config) {
+  NProgress.start()
+  setInterval(() => {
+    NProgress.inc()
+  }, 150)
+  return config;
+}, function (error) {
+  NProgress.done()
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use(function (response) {
+  NProgress.done()
+  return response;
+}, function (error) {
+  NProgress.done()
+  return Promise.reject(error);
+});
+
 
 export const getRealtimeDataFor = siteId => {
   return axios.get('/.netlify/functions/realTimeApi?siteId=' + siteId)
