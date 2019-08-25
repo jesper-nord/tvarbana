4e2436050ee4ea44824cc2b28b42a773121815c1
Jesper Lehtinen <jsprlehtinen@gmail.com>
2019-08-25 15:46:02 +0200

    adds lambda server api to solve cors problems

---
+++ src/lambda/realTimeApi.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const BASE_URL = 'https://api.sl.se/api2'
+const REAL_TIME_API = 'realtimedeparturesV4.json'
+const { REACT_APP_SL_API_KEY } = process.env;
+
+exports.handler = async (event, context) => {
+  const siteId = event.queryStringParameters.siteId
+  const url = `${BASE_URL}/${REAL_TIME_API}?key=${REACT_APP_SL_API_KEY}&siteid=${siteId}&timewindow=30`
+
+  return axios.get(url, { headers: { "Accept": "application/json" } })
+    .then(response => response.json())
+    .then(data => ({
+      statusCode: 200,
+      body: data
+    }))
+    .catch(error => ({ statusCode: 400, body: String(error) }));
+};
--- src/service.js
+++ src/service.js
@@ -1,8 +1,5 @@
 import axios from 'axios'
 
-const BASE_URL = 'https://api.sl.se/api2'
-const REAL_TIME_API = 'realtimedeparturesV4.json'
-
 export const getRealtimeDataFor = siteId => {
-  return axios.get(`${BASE_URL}/${REAL_TIME_API}?key=${process.env.REACT_APP_SL_API_KEY}&siteid=${siteId}&timewindow=30`)
+  return axios.get('/.netlify/functions/realTimeApi?siteId=' + siteId)
 }
